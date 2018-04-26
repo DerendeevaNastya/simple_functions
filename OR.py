399ed8e3306a8b566a74c5dda8cbc67fd8826433
DerendeevaNastya <32324515+DerendeevaNastya@users.noreply.github.com>
2018-04-27 00:50:13 +0500

    azaza

--- OR.py
+++ OR.py
@@ -1,18 +1,23 @@
 from perseptron import Perseptron
+import random
+
 
 def like_sgn(res):
-    return 1 if res > -0.000001 else -1
+    return 1 if res >= 0.5 else 0
+
 
 def learning():
     pers = Perseptron(like_sgn, 2)
     examples = [[1, 1, 1],
-                [-1, 1, 1],
-                [1, -1, 1],
-                [-1, -1, -1]]
-    for i in range(0, 5):
-        for example in examples:
-            print("{0} {1} : {3}, correct {2}".format(*example, pers.get_output([example[0], example[1], 1])))
-            pers.correct_weight(example[2])
+                [0, 1, 1],
+                [1, 0, 1],
+                [0, 0, 0]]
+    for _ in range(0, 1000):
+        j = random.randint(0, 3)
+        result = pers.get_output([examples[j][0], examples[j][1]])
+        print("{0} {1} : {3}, correct {2}".format(*examples[j], result))
+        #result = like_sgn(result)
+        pers.correct_weight(examples[j][2], result)
 
 
 learning()
--- perseptron.py
+++ perseptron.py
@@ -3,6 +3,7 @@ import random
 
 class Perseptron:
     def __init__(self, converter, dendrites_count):
+        self.dendrites_count = dendrites_count
         self.dendrites_weight = [random.random() for _ in range(dendrites_count + 1)]
         self.epsilon = 0.2
         self.input = []
@@ -11,16 +12,17 @@ class Perseptron:
 
     def get_output(self, input_data):
         result = 0
-        self.input = input_data
+        self.input = input_data + [1]
         for i in range(0, len(input_data)):
             result += input_data[i] * self.dendrites_weight[i]
         result += self.dendrites_weight[-1]
-        self.axon = self.converter(result)
+        print(result)
+        result = self.converter(result)
         return result
 
-    def correct_weight(self, correct_result):
-        d = correct_result - self.axon
-        for i in range(len(self.dendrites_weight)):
-            delta = self.epsilon * d * self.input[i]# * self.dendrites_weight[i]
+    def correct_weight(self, correct_result, result):
+        d = correct_result - result
+        for i in range(self.dendrites_count):
+            delta = self.epsilon * d * self.input[i]
             self.dendrites_weight[i] += delta
             self.dendrites_weight[i] += -0.1 if self.dendrites_weight[i] == 0 else 0
