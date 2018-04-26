2a5564993dc8da500d2b9c0666ae3eee87a7d367
1MiKHalyCH1 <bykoun@gmail.com>
2018-04-27 01:38:55 +0500

    some fixes

--- perseptron.py
+++ perseptron.py
@@ -2,27 +2,22 @@ import random
 
 
 class Perseptron:
-    def __init__(self, converter, dendrites_count):
-        self.dendrites_count = dendrites_count
-        self.dendrites_weight = [random.random() for _ in range(dendrites_count + 1)]
+    def __init__(self, activation, dendrites_count):
+        self.weights = [random.random() for _ in range(dendrites_count + 1)]
         self.epsilon = 0.2
-        self.input = []
-        self.converter = converter
-        self.axon = 0
+        self.activation = activation
+        self.bias = 1
 
     def get_output(self, input_data):
         result = 0
-        self.input = input_data + [1]
-        for i in range(0, len(input_data)):
-            result += input_data[i] * self.dendrites_weight[i]
-        result += self.dendrites_weight[-1]
-        print(result)
-        result = self.converter(result)
-        return result
+        input_data = list(input_data) + [self.bias]
+        for i in range(len(input_data)):
+            result += input_data[i] * self.weights[i]
+        result += self.weights[-1]
+        return self.activation(result)
 
-    def correct_weight(self, correct_result, result):
-        d = correct_result - result
-        for i in range(self.dendrites_count):
-            delta = self.epsilon * d * self.input[i]
-            self.dendrites_weight[i] += delta
-            self.dendrites_weight[i] += -0.1 if self.dendrites_weight[i] == 0 else 0
+    def correct_weight(self, X, y, result):
+        delta = y - result
+        for i in range(len(X)):
+            self.weights[i] += self.epsilon * delta * X[i]
+        self.weights[-1] += self.epsilon * delta * self.bias
