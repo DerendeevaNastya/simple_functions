fc207dabdea03ca0392bd9a9e795364cc284fa42
DerendeevaNastya <32324515+DerendeevaNastya@users.noreply.github.com>
2018-04-26 21:49:44 +0500

    lalala

---
+++ AND.py
@@ -0,0 +1,17 @@
+from perseptron import Perseptron
+
+def learning():
+    pers = Perseptron()
+    examples = [[1, 1, 1],
+                [-1, 1, -1],
+                [1, -1, -1],
+                [-1, -1, -1]]
+    for i in range(0, 5):
+        for example in examples:
+            print("{0} {1} : {3}, correct {2}".format(*example, pers.get_output([example[0], example[1], 1])))
+            pers.correct_weight(example[2])
+
+
+learning()
+
+
---
+++ perseptron.py
@@ -0,0 +1,22 @@
+class Perseptron:
+    def __init__(self):
+        self.dendrit_weight = [1, 1, 1]
+        self.epsilon = 0.2
+        self.input = []
+        self.acson = 0
+
+    def get_output(self, input_data):
+        result = 0
+        self.input = input_data
+        for i in range(0, len(input_data)):
+            result += input_data[i]*self.dendrit_weight[i]
+        result += self.dendrit_weight[-1]
+        self.acson = 1 if result > -0.000001 else -1
+        return result
+
+    def correct_weight(self, correct_result):
+        d = correct_result - self.acson
+        for i in range(len(self.dendrit_weight)):
+            delta = self.epsilon * d * self.input[i]# * self.dendrit_weight[i]
+            self.dendrit_weight[i] += delta
+            self.dendrit_weight[i] += -0.1 if self.dendrit_weight[i] == 0 else 0
